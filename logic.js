b4997127d42befac33fb5ce94f5ec2db7ffab225
Rudyard <rudyard90@gmail.com>
2020-11-08 22:53:43 -0600

    Add array of quotes Lorem ipsum

---
+++ logic.js
@@ -0,0 +1,51 @@
+// Get all quotes
+const quotes = [
+    {
+        quote: 
+         'Lorem ipsum dolor sit amet consectetur adipiscing elit elementum',
+        author: "Rudyard walkinshaw"
+    },
+    {
+        quote: 
+         'Lorem ipsum dolor sit amet consectetur adipiscing elit elementum',
+        author: "Rudyard walkinshaw"
+    },
+    {
+        quote: 
+         'Lorem ipsum dolor sit amet consectetur adipiscing elit elementum',
+        author: "Rudyard walkinshaw"
+    },
+    {
+        quote: 
+         'Lorem ipsum dolor sit amet consectetur adipiscing elit elementum',
+        author: "Rudyard walkinshaw"
+    },
+    {
+        quote: 
+         'Lorem ipsum dolor sit amet consectetur adipiscing elit elementum',
+        author: "Rudyard walkinshaw"
+    },
+    {
+        quote: 
+         'Lorem ipsum dolor sit amet consectetur adipiscing elit elementum',
+        author: "Rudyard walkinshaw"
+    },
+    {
+        quote: 
+         'Lorem ipsum dolor sit amet consectetur adipiscing elit elementum',
+        author: "Rudyard walkinshaw"
+    },
+    {
+        quote: 
+         'Lorem ipsum dolor sit amet consectetur adipiscing elit elementum',
+        author: "Rudyard walkinshaw"
+    },
+];
+
+//Button Generate Quotes
+const btn = document.querySelector('.generate');
+
+//Add Event Listener
+
+
+console.log(quotes);
